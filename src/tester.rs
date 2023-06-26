b078e45b11bfa64ed82a5e9c668763a0d8d5e978
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-06-26 01:43:28 -0400

    best tester

--- src/tester.rs
+++ src/tester.rs
@@ -1,6 +1,10 @@
+use std::cmp::{max, min};
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+
+extern crate colored;
+use colored::*;
 
 use crate::languages::Language;
 
@@ -44,23 +48,31 @@ pub fn run_test(
 
     match r {
         Ok(out) => {
-            let check = check_out(output, out)?;
-
-            if check {
-                println!("OK");
-            } else {
-                println!("Failed");
-            }
+            check_out(output, out)?;
         }
         Err(e) => {
-            println!("Error {}", e);
+            println!("{}: {}", "Error:\n".red(), e);
         }
     };
 
     Ok(())
 }
 
-fn check_out(output: String, out: String) -> Result<bool, String> {
+fn compare(output: &Vec<&str>, out: &Vec<&str>) -> bool {
+    if output.len() != out.len() {
+        return false;
+    }
+
+    for (s1, s2) in output.into_iter().zip(out.into_iter()) {
+        if s1 != s2 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn check_out(output: String, out: String) -> Result<(), String> {
     let mut output = File::open(output).unwrap();
 
     let mut content = String::new();
@@ -71,5 +83,36 @@ fn check_out(output: String, out: String) -> Result<bool, String> {
         }
     }
 
-    Ok(content == out)
+    let output: Vec<_> = content.split("\n").collect();
+    let out: Vec<_> = out.split("\n").collect();
+
+    let output: Vec<_> = output.into_iter().map(|s| s.trim_end()).collect();
+    let out: Vec<_> = out.into_iter().map(|s| s.trim_end()).collect();
+
+    if compare(&output, &out) {
+        println!("{}", "OK".green());
+    } else {
+        println!("{}", "Failed:\n".red());
+        analyze_failed(&output, &out);
+    }
+
+    Ok(())
+}
+
+fn analyze_failed(output: &Vec<&str>, out: &Vec<&str>) {
+    let mut output: Vec<_> = output.clone();
+    let mut out: Vec<_> = out.clone();
+
+    let dif = max(output.len(), out.len()) - min(output.len(), out.len());
+    output.extend(vec![""; dif]);
+    out.extend(vec![""; dif]);
+
+    for (s1, s2) in output.into_iter().zip(out.into_iter()) {
+        if s1 == s2 {
+            println!("{}", s1);
+        } else {
+            println!("{}", s1.green());
+            println!("{}", s2.red());
+        }
+    }
 }
